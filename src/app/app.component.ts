32dbd81752af2db0ef17bab25b0a5389c79aee2a
dragonslayer587 <kevinwainz587@gmail.com>
2021-07-27 18:42:15 -0300

    ejercicio Moi

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'test-angular-app';
+  result:number = 0;
+  operation: {
+    number1:number,
+    number2:number,
+    type:string,
+  } = {number1:0, number2:0, type:"sum"}
+  
+  doOperation(){
+    this.operation.number1 = parseInt(((document.getElementById("number1") as HTMLInputElement).value));
+    this.operation.number2 = parseInt(((document.getElementById("number2") as HTMLInputElement).value));
+    this.operation.type = ((document.getElementById("operationType") as HTMLSelectElement).value);
+    switch (this.operation.type) {
+      case 'add':
+        this.result = this.operation.number1 + this.operation.number2
+        break
+      case 'subtract':
+        this.result = this.operation.number1 - this.operation.number2
+        break
+      case 'multiply':
+        this.result = this.operation.number1 * this.operation.number2
+        break
+    }
+
+  }
 }
